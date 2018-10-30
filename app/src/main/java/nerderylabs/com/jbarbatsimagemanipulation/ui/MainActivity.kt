f5e28a29cff1980944a8467b65643dc0d258d496
Peter Barbatsis <pbarbats@openup0-42.bravo>
2018-10-30 01:50:18 -0500

    deleted the recycler view, created touch class

--- app/src/main/java/nerderylabs/com/jbarbatsimagemanipulation/ui/MainActivity.kt
+++ app/src/main/java/nerderylabs/com/jbarbatsimagemanipulation/ui/MainActivity.kt
@@ -6,18 +6,11 @@ import nerderylabs.com.jbarbatsimagemanipulation.R
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var imageAdapter: ImageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        setupRecycler()
     }
 
-    private fun setupRecycler() {
 
-        imageAdapter = ImageAdapter(this)
-
-    }
 }
